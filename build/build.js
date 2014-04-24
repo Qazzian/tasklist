44563cf80d5847bdd27aaaf0eef153d953f33e8b
Qazzian <Qazzian@gmail.com>
2014-04-24 23:34:14 +0100

    compiled components

---
+++ build/build.js
@@ -0,0 +1,70 @@
+/**
+ * Require the module at `name`.
+ *
+ * @param {String} name
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(name) {
+  var module = require.modules[name];
+  if (!module) throw new Error('failed to require "' + name + '"');
+
+  if (module.definition) {
+    module.client = module.component = true;
+    module.definition.call(this, module.exports = {}, module);
+    delete module.definition;
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Register module at `name` with callback `definition`.
+ *
+ * @param {String} name
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function (name, definition) {
+  require.modules[name] = {
+    definition: definition
+  };
+};
+
+/**
+ * Define a module's exports immediately with `exports`.
+ *
+ * @param {String} name
+ * @param {Generic} exports
+ * @api private
+ */
+
+require.define = function (name, exports) {
+  require.modules[name] = {
+    exports: exports
+  };
+};
+
+require.register("tasklist", function (exports, module) {
+/**
+ * Created by Ian on 04/04/2014.
+ */
+
+var p = document.querySelector('p');
+
+setInterval(function () {
+	p.style.visibility = getComputedStyle(p).visibility === 'hidden'
+			? 'visible'
+			: 'hidden';
+}, 300);
+});
+
+require("tasklist")
